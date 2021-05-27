17b38fba43905eed1319c4e4948d83dc74959dd0
Alina T <alina1989malina@yandex.ru>
2021-05-27 11:28:57 +0300

    add more functions to testin example app

--- example/App.js
+++ example/App.js
@@ -50,7 +50,11 @@ export default class App extends Component<{}> {
         </View>
         <View>
               <Button onPress={() => nfcCardModuleWrapper.getKeyChainInfo()
-            .then((result) => alert("getKeyChainInfo : " + result.message)).catch((e) => alert(e.message))} title="getKeyChainInfo"/>
+            .then((result) => alert("getKeyChainInfo : " + result.numberOfKeys)).catch((e) => alert(e.message))} title="getKeyChainInfo"/>
+        </View>
+        <View>
+              <Button onPress={() => nfcCardModuleWrapper.getHmac("0")
+            .then((result) => alert("getHmac : " + result.hmac)).catch((e) => alert(e.message))} title="getHmac"/>
         </View>
         <View>
               <Button onPress={() => nfcCardModuleWrapper.getPublicKeyForDefaultPath()
@@ -88,7 +92,16 @@ export default class App extends Component<{}> {
         <View>
               <Button onPress={() => nfcCardModuleWrapper.getMaxPinTriesWithoutDialog()
             .then((result) => alert("getMaxPinTries : " + result.message)).catch((e) => alert(e.message))} title="getMaxPinTries"/>
-        </View>    
+        </View>   
+
+        <View>
+              <Button onPress={() => nfcCardModuleWrapper.createKeyForHmac(
+                "F4B072E1DF2DB7CF6CD0CD681EC5CD2D071458D278E6546763CBB4860F8082FE14418C8A8A55E2106CBC6CB1174F4BA6D827A26A2D205F99B7E00401DA4C15ACC943274B92258114B5E11C16DA64484034F93771547FBE60DA70E273E6BD64F8A4201A9913B386BCA55B6678CFD7E7E68A646A7543E9E439DD5B60B9615079FE",
+                "7256EFE7A77AFC7E9088266EF27A93CB01CD9432E0DB66D600745D506EE04AC4",
+                "504394802433901126813236"
+              )
+            .then((result) => alert("createKeyForHmac : " + result.message)).catch((e) => alert(e.message))} title="createKeyForHmac"/>
+        </View>
       
       </View>
       
